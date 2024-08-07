83cb5ce3052b1e1cd00b6da848214e36139f1c55
ZevDev54 <zevy.johnston@gmail.com>
2024-08-07 17:42:14 -0600

    added construction style

--- src/app/dirk-durnston/layout.js
+++ src/app/dirk-durnston/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import styles from "./style.module.scss";
+// import '../globals.scss'
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import styles from './style.module.scss'
+import './globals.scss'
 
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className={styles.centerItems}>
+        <h1 className="construction">This page is under construction!</h1>
         <h1>Welcome to ZIWE.</h1>
         <h2>We mean business.</h2>
+        <p>Promoting small local businesses is what we do. We are dedicated to spreading the word for up-and-coming businesses by way of professional busienss cards, custom websites, posters, and more.</p>
       </div>
     </main>
   );
