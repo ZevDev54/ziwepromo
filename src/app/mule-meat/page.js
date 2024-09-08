97403557062696d0533e15c5ba16e67f00a54972
ZevDev54 <zevy.johnston@gmail.com>
2024-09-07 20:03:09 -0600

    fix apostrophe character killing the compiler

--- src/app/mule-meat/page.js
+++ src/app/mule-meat/page.js
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <main>
       <h1 className={squada.className}>Mule meat</h1>
-      <h2 className={squada.className}>What you've been craving.</h2>
+      <h2 className={squada.className}>What you have been craving.</h2>
       <h2 className={squada.className}>This page is under cosntruction. You'll have to hold out a bit longer for mule meat!</h2>
 
     
